7218ba1604c83ef7bdf438e8e77684314d6d46e9
Oscar Gichana <ossygicchy@gmail.com>
2021-04-06 18:28:33 +0300

    adding index page views

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http  import HttpResponse
+import datetime as dt
+from django.http  import HttpResponse,Http404
 
 # Create your views here.
+def index(request):
+    # images = Image.get_all_images()
+    # locations = Location.objects.all()
+    title = 'O_world'
+    return render(request, 'index.html', )
