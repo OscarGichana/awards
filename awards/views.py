3d6446d7cc20abe1317f9566102c12dfe4a4cc92
Oscar Gichana <ossygicchy@gmail.com>
2021-04-08 13:21:31 +0300

    ading api views

--- awards/views.py
+++ awards/views.py
@@ -161,5 +161,91 @@ def search_projects(request):
 
 
 
+class MerchList(APIView):
+    def get(self, request, format=None):
+        all_merch = MoringaMerch.objects.all()
+        serializers = MerchSerializer(all_merch, many=True)
+        return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = MerchSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class ProjectList(APIView):
+    def get(self, request, format=None):
+        all_merch = AwardsProject.objects.all()
+        serializers = ProjectSerializer(all_merch, many=True)
+        return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ProjectSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class MerchDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_merch(self, pk):
+        try:
+            return MoringaMerch.objects.get(pk=pk)
+        except MoringaMerch.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        merch = self.get_merch(pk)
+        serializers = MerchSerializer(merch)
+        return Response(serializers.data)
+
+    def put(self, request, pk, format=None):
+        merch = self.get_merch(pk)
+        serializers = MerchSerializer(merch, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        merch = self.get_merch(pk)
+        merch.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ProductDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_prod(self, pk):
+        try:
+            return AwardsProject.objects.get(pk=pk)
+        except AwardsProject.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        prod = self.get_prod(pk)
+        serializers = ProductSerializer(merch)
+        return Response(serializers.data)
+
+    def put(self, request, pk, format=None):
+        prod = self.get_prod(pk)
+        serializers = ProductSerializer(prod, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        prod = self.get_prod(pk)
+        prod.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
