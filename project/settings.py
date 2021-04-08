7f35ad03154649d42601caa5cf5075e9bca6ffb8
Oscar Gichana <ossygicchy@gmail.com>
2021-04-08 14:15:27 +0300

    changing database password

--- project/settings.py
+++ project/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'awards',
         'USER': 'oscar',
-        'PASSWORD':'123',
+        'PASSWORD':'123456789os',
         'DISABLE_SERVER_SIDE_CURSORS': True,
 
     }
